abad3a6020e6298207c84294c42065fa2490e42a
Abga001 <abdullahagumel@gmail.com>
2022-08-04 08:58:23 +0100

    Second commit

---
+++ Function1.js
@@ -0,0 +1,18 @@
+'use strict'
+
+function twoParameters(num1, num2) {
+    return num1 - num2;
+
+}
+
+console.log(twoParameters(10,2));
+
+function multiple(welcome,name,age,gender){
+    return `${welcome} my name is ${name}, I am ${age} years old and of gender ${gender}.`
+}
+
+console.log(multiple("Hi","Jack","22","Male"))
+
+powerUP = (n1, n2) => Math.pow(n1,n2);
+
+console.log(powerUp(2,3));
